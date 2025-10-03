846220643a04d10a36567ef53508257979ec7fcb
Alex Halverson <alexhalverson@posteo.net>
2025-10-03 12:40:55 +1000

    feat(artwork): hook up sketch to recieve ColourPickerHub requests

--- wwwroot/js/sketch.js
+++ wwwroot/js/sketch.js
@@ -1,7 +1,35 @@
+let pickerConnection;
+let userString;
+let colourString;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  connectSignalR();
+}
+
+async function connectSignalR() {
+  pickerConnection = new signalR.HubConnectionBuilder()
+    .withUrl('/colourPickerHub')
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+  pickerConnection.on('ReceiveMessage', function (user, message) {
+    userString = user;
+    colourString = message;
+  });
+  try {
+    await pickerConnection.start();
+  } catch (err) {
+    console.error(err.toString());
+  }
 }
 
 function draw() {
+  if (colourString) {
+    fill(colourString);
+    stroke(colourString);
+  } else {
+    noFill();
+  }
   circle(50, 50, 30);
 }
